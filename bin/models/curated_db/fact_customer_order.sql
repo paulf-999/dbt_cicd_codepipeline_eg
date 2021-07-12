314069ae6ec61d3b5b8f2d3d9039a7bae4b0f864
Paul Fry <paul.fry@versent.com.au>
2021-07-12 14:54:47 +1000

    repo housekeeping of model files

--- bin/models/curated_db/fact_customer_order.sql
+++ bin/models/curated_db/fact_customer_order.sql
@@ -2,15 +2,13 @@ SELECT order_items.order_id
         , products.product_id
         , stores.store_id
         , staffs.staff_id
-        , products.product_name        
+        , products.product_name
         , order_date
         , required_date
         , shipped_date
         , order_items.quantity
         , order_items.list_price
         , order_items.discount
-        , staffs.first_name
-        , staffs.last_name
 FROM {{ source('bike_production', 'products') }}
 LEFT JOIN {{ source('bike_sales', 'order_items') }}
     ON order_items.product_id = {{ source('bike_production', 'products') }}.product_id
